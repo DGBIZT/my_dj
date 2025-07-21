10f918609a04bca81688fe6d5b38a96e4c21dffa
DGBIZT <georgiy.gorlov@gmail.com>
2025-07-21 14:00:27 +0300

    Переходим к примеру работы со статическими файлами

--- config/urls.py
+++ config/urls.py
@@ -18,7 +18,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('students/', include('students.urls', namesspace='students') )
+    # path('students/', include('students.urls', namesspace='students') )
+    path('students/', include('students.urls', namespace='students')),
 ]
--- students/views.py
+++ students/views.py
@@ -2,21 +2,33 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def example_view(request):
-    return render(request, 'app/example.html')
+# def example_view(request):
+#     return render(request, 'app/example.html')
+#
+# def show_data(request):
+#     if request.method == 'GET':
+#         return render(request, 'app/show_data.html')
+#
+# def submit_data(request):
+#     # request.GET
+#     # request.POST
+#     if request.method == 'POST':
+#         return  HttpResponse("Данные отправлены")
+#
+#
+# def show_item(request, item_id):
+#     return render(request, 'app/item.html', {'item_id': item_id})
+#
 
-def show_data(request):
-    if request.method == 'GET':
-        return render(request, 'app/show_data.html')
 
-def submit_data(request):
-    # request.GET
-    # request.POST
+def about(request):
+    return render(request, 'students/about.html')
+
+def contact(request):
     if request.method == 'POST':
-        return  HttpResponse("Данные отправлены")
+        name = request.POST.get('name')
+        message = request.POST.get('message')
 
+        return HttpResponse(f'Спасибо, {name}! Сообщение получено.')
 
-def show_item(request, item_id):
-    return render(request, 'app/item.html', {'item_id': item_id})
-
-
+    return render(request, 'students/contact.html')
