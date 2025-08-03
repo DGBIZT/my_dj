33fab29163f495fa5c8df377c207b757c82c60a5
DGBIZT <georgiy.gorlov@gmail.com>
2025-08-03 20:29:59 +0300

    создания шаблона для приложения library

--- library/urls.py
+++ library/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import books_list, book_detail
+from .views import books_list, book_detail, base
 
 
 app_name = 'library'
@@ -7,4 +7,5 @@ app_name = 'library'
 urlpatterns = [
     path('books_list/', books_list, name='books_list'),
     path("book_detail/<int:book_id>", book_detail, name='book_detail'),
+    path('base/', base, name='base'),
 ]
--- library/views.py
+++ library/views.py
@@ -11,3 +11,8 @@ def book_detail(request, book_id):
     book = Book.objects.get(id=book_id)
     context = {'book': book}
     return render(request, 'library/book_detail.html', context)
+
+def base(request):
+    books = Book.objects.all()
+    context = {'books': books}
+    return render(request, 'library/base.html', context)
