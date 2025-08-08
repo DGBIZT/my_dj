ae6797b5b51085c77778fe51fa887e5e007085c5
George Gorlov <Georgiy.Gorlov@gmail.com>
2025-08-08 15:00:48 +0300

    Методы классов в CBV

--- students/views.py
+++ students/views.py
@@ -13,16 +13,48 @@ class MyModelCreteView(CreateView):
     template_name = 'students/mymodel_form.html'
     success_url = reverse_lazy('students:mymodel_list')
 
+    def form_valid(self, form):
+        form.instance.create_by = self.request.user
+
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        response = super().form_invalid(form)
+        response.context_data['error_message'] = "Please correct the errors"
+
+        return response
+
+
 
 class MyModelListView(ListView):
     model = MyModel
     template_name = 'students/mymodel_list.html'
     context_object_name = 'mymodels'
 
+    def get_queryset(self):
+
+        return MyModel.objects.filter(is_active=True)
+
+
 class MyModelDetailView(DetailView):
     model = MyModel
     TemplateName = 'students/mymodel_detail.html'
     context_object_name = 'mymodel'
+
+    def get_additional_data(self):
+        return 'Это дополнительная информация'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['additional_data'] = self.get_additional_data()
+        return context
+
+    def get_object(self, queryset =None):
+        obj = super().get_object(queryset)
+        if not obj.is_active:
+            raise Http404("Object not found")
+        return obj
+
 
 class MyModelUpdateView(UpdateView):
     model = MyModel
