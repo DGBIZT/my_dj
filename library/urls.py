cda207f191e2cd7c61852f136df4245813d5e0c0
DGBIZT <georgiy.gorlov@gmail.com>
2025-08-09 17:15:42 +0300

    Практическая работа с library Реализация CRUDопераций для модели

--- library/urls.py
+++ library/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
-from .views import books_list, book_detail, base
+from .views import BooksListView, BookUpdateView, BookCreateView, BookDeleteView, BookDetailView
 
 
 app_name = 'library'
 
 urlpatterns = [
-    path('books_list/', books_list, name='books_list'),
-    path("book_detail/<int:book_id>", book_detail, name='book_detail'),
-    path('base/', base, name='base'),
+    path('books/', BooksListView.as_view(), name='books_list'),
+    path('book/new/', BookCreateView.as_view(), name='book_create'),
+    path('book/<int:pk>/', BookDetailView.as_view(), name='book_detail'),
+    path('book/update/<int:pk>/', BookUpdateView.as_view(), name='book_update'),
+    path('book/delete/<int:pk>/', BookDeleteView.as_view(), name='book_delete'),
+
 ]
